a2068e94589bbd25a73696f6b990c7453eb86a79
jonesfreedman <jonesfreedman@gmail.com>
2020-04-19 20:28:17 +0530

    Initial commit

---
+++ FilesSorter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FilesSorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter the path: ");
+                var path = Console.ReadLine();
+                Sort sort = new Sort(path);
+                Console.WriteLine("Sort successful!");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error encountered " + ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ FilesSorter/Sort.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FilesSorter
+{
+    public class Sort
+    {
+        readonly string _path;
+        public Sort(string path)
+        {
+            _path = path;
+            CreateFolderAndMoveFiles();
+        }
+
+        private List<string> GetAllFiles()
+        {
+            return Directory.GetFiles(_path).ToList();
+        }
+
+        private List<string> GetExistingExtension()
+        {
+            var files = GetAllFiles();
+            List<string> extensions = new List<string>();
+            extensions.AddRange(files.Select(file => Path.GetExtension(file)).Distinct());
+            return extensions;
+        }
+
+        private void CreateFolderAndMoveFiles()
+        {
+            var extensions = GetExistingExtension();
+            foreach (var extension in extensions)
+            {
+                var path = string.Empty;
+                if (!string.IsNullOrEmpty(extension))
+                {
+                    var ext = extension.Remove(0, 1);
+                    path = _path + "/" + ext.Trim().ToUpper();
+                }
+                else
+                {
+                    path = _path + "/" + Constants.FILEWITHOUTEXTENSION;
+                }
+                Directory.CreateDirectory(path);
+                MoveFiles(extension, path);
+            }
+        }
+
+        private void MoveFiles(string extension, string path)
+        {
+            var files = GetAllFiles();
+            var specificFiles = files.Where(r => r.EndsWith(extension)).Select(r => r);
+            foreach (var file in specificFiles)
+            {
+                var fileName = Path.GetFileName(file);
+                var newLocation = path + "/" + fileName;
+                File.Move(file, newLocation, false);
+            }
+        }
+    }
+}
